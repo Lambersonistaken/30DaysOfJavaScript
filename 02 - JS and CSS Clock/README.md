f4897fdaa729804c6c6dce2737349b2bf131c2be
İsmail Emir Lambacıoğlu <73862428+Lambersonistaken@users.noreply.github.com>
2023-01-30 18:51:44 +0300

    Add files via upload

---
+++ 02 - JS and CSS Clock/README.md
@@ -0,0 +1,5 @@
+# JavaScript and CSS Clock by Lamberson
+
+In this project I have built a JavaScript and CSS clock. The clock is built using HTML, CSS and JavaScript. The clock is built using the `transform` property in CSS and the `setInterval()` method in JavaScript.
+
+# Screenshots
---
+++ 02 - JS and CSS Clock/index.js
@@ -0,0 +1,25 @@
+
+// getting the hands
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+// getting the time
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hours = now.getMinutes();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+
+}
+// calling the function
+setInterval(setDate, 1000);
